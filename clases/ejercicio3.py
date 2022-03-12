262c9c2891003bfa640e5aa3e4069ea0e32ef1f1
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-03-12 15:01:46 +0100

    ejr 3

---
+++ clases/ejercicio3.py
@@ -0,0 +1,59 @@
+class ejr3():
+    def __init__(self):
+        while True:
+            vector = input("Escriba los elementos de su vector separados por espacios: ")
+            vector = vector.split()
+            try:
+                for i in range(len(vector)):
+                    vector[i] = int(vector[i])
+            except:
+                try:
+                    for i in range(len(vector)):
+                        vector[i] = float(vector[i])
+                    print("Los ordenaremos como tipo float")
+                except:
+                    print("Los ordenaremos como tipo string")
+            break
+        s1 = []
+        s2 = []
+        self.vector = vector
+        self.s1 = s1
+        self.s2 = s2
+
+
+    def segmentos(self):
+        d1 = max(self.vector[:int(len(self.vector)/2)])
+        d2 = ''
+        self.s1.append(d1)
+        for i in range(len(self.vector) - self.vector.index(d1)):
+            i = i + self.vector.index(d1) + 1
+            if i != len(self.vector):
+                if self.vector[i] <= d1:
+                    self.s1.append(self.vector[i])
+                else:
+                    d2 = self.vector[i]
+                    break
+            else:
+                break
+        self.s2.append(d2)
+        if self.s2 != ['']:
+            for i in range(len(self.vector) - self.vector.index(d1)):
+                i = i + self.vector.index(d2) + 1
+                if i != len(self.vector):
+                    if self.vector[i] <= d2:
+                        self.s2.append(self.vector[i])
+                    else:
+                        d2 = self.vector[i]
+                        break
+                else:
+                    break
+
+    def resultado(self):
+        self.segmentos()
+        print("Este es el primer vector:")
+        print(self.s1)
+        if self.s2 != ['']:
+            print("Este es el segundo vector:")
+            print(self.s2)
+        else:
+            print("Solo hay un vector")
